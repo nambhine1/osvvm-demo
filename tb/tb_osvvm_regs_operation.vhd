963a151053b79d7c22ad590ff57f147d7185d2a8
Nambinina Rakotojaona <nambinina.rakotojaona@plc2.de>
2025-06-17 23:19:28 +0200

    change test cases

--- tb/tb_osvvm_regs_operation.vhd
+++ tb/tb_osvvm_regs_operation.vhd
@@ -34,8 +34,6 @@ use osvvm.ScoreboardPkg_slv.all;
 library osvvm_axi4;
 use osvvm_axi4.Axi4OptionsPkg.all;
 
-use work.osvvm_regs_pkg.all;            -- contains the register bank address definitions
-
 architecture operation of tb_osvvm_regs_testctrl is
 
 begin
@@ -45,7 +43,7 @@ begin
     --   Set up AlertLog and wait for end of test
     ------------------------------------------------------------
     ControlProc : process
-        variable addr           : unsigned(31 downto 0);
+        variable addr           : std_logic_vector(31 downto 0);
         variable wdata          : std_logic_vector(31 downto 0);
         variable rdata          : std_logic_vector(31 downto 0);
         variable actual_value   : std_logic_vector(STATUS_VALUE_BIT_WIDTH - 1 downto 0);
@@ -65,19 +63,19 @@ begin
         ClearAlerts;
 
         Log("Write Control register");
-        addr  := unsigned(REGS_BASEADDR) + CONTROL_OFFSET;
+        addr  := x"00000008"
         wdata := x"00001234";
-        Write(Axi4MemRec, std_logic_vector(addr), wdata);
+        Write(Axi4MemRec, addr, wdata);
 
         Log("Read Status register");
-        Read(Axi4MemRec, std_logic_vector(addr), rdata);
+        Read(Axi4MemRec, addr, rdata);
 
         -- Extract field value from register value
-        actual_value   := rdata(STATUS_VALUE_BIT_WIDTH + STATUS_VALUE_BIT_OFFSET - 1 downto STATUS_VALUE_BIT_OFFSET);
-        expected_value := wdata(CONTROL_VALUE_BIT_OFFSET + CONTROL_VALUE_BIT_WIDTH - 1 downto CONTROL_VALUE_BIT_OFFSET);
+        --actual_value   := rdata(STATUS_VALUE_BIT_WIDTH + STATUS_VALUE_BIT_OFFSET - 1 downto STATUS_VALUE_BIT_OFFSET);
+        --expected_value := wdata(CONTROL_VALUE_BIT_OFFSET + CONTROL_VALUE_BIT_WIDTH - 1 downto CONTROL_VALUE_BIT_OFFSET);
 
         -- Compare write and read field values
-        AffirmIfEqual(expected_value, actual_value, "read data");
+        AffirmIfEqual(x"00001234", rdata, "read data");
 
         EndOfTestReports;
         std.env.stop;
